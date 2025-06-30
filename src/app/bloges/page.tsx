631cd223a2ce61c396f31aeb631da9d5b4d27996
Roger <kongnyuroger711@gmail.com>
2025-06-30 11:37:47 +0100

    created 404 page

---
+++ src/app/bloges/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Bloge = () => {
+  return (
+    <div>Bloge page</div>
+  )
+}
+
+export default Bloge
---
+++ src/app/products/[productid]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const product = () => {
+  return (
+    <div>product details</div>
+  )
+}
+
+export default product
